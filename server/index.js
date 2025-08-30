86306b27387ebd2a6089bb665b3be6d6e88ce170
ADITYA K <adityak20050411@gmail.com>
2025-08-30 19:50:49 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -113,3 +113,14 @@ process.on('SIGTERM', () => {
     process.exit(0);
   });
 });
+
+const path = require("path");
+
+// Serve React build files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
